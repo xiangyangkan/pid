7b9113d7807dfa4156fb995a2cafe2310b77a3f4
xiangyangkan <xiangyangkan@outlook.com>
2019-08-21 16:02:34 +0800

    update

--- setup.py
+++ setup.py
@@ -40,6 +40,9 @@ setuptools.setup(
     ],
     keywords='pid controller control',
     packages=setuptools.find_packages(),
+    extras_require={
+        'docs': ['m2r', 'sphinx-rtd-theme']
+    },
     project_urls={
         'Documentation': 'https://mix-pid.readthedocs.io/',
     },
